02f6f6b98bdd2029d1f8b9662482ce60772dfa8e
tahminulislam <50363221+tahminulislam@users.noreply.github.com>
2020-10-09 03:24:10 +0600

    Tetris

---
+++ Tetris-Game/Assets/score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+
+{
+    public static int scoreval = 0;
+    Text scoreboard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreboard = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreboard.text = "Score : " + scoreval;
+    }
+}
